80ea89b85a09806b6f37d7def26c201d9becc053
zhuss <2734034714@qq.com>
2019-12-31 15:06:44 +0800

    ts5 v1.0

---
+++ src/main/java/com/baizhi/ts5/T1.java
@@ -0,0 +1,13 @@
+package com.baizhi.ts5;
+
+/**
+ * @ClassName T1
+ * @Author
+ * @Date 2019/12/31 15:02
+ * @Version 1.0
+ **/
+public class T1 {
+    public void m1() {
+        System.out.println("测试");
+    }
+}
---
+++ src/test/java/com/baizhi/ts5/Ts5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baizhi.ts5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ts5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
